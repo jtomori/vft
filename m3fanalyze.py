5ab1f0ac898d64d8cf0b80fc7dd4af2a7fc46c2b
jtomori <tomori.juraj@gmail.com>
2017-12-11 19:13:55 +0000

    sorts MB3D .m3f files based on number of instructions

---
+++ m3fanalyze.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+import fnmatch
+
+def countInstructions(filePath):
+	with open(filePath) as file:
+		lines = file.readlines()
+		lines = [n.replace(" ","") for n in lines] # kill spaces for finding exact line
+
+		# find start and ending lines of code section
+		start = lines.index("[CODE]\n")
+		end = lines.index("[END]\n")
+		length = end-start-1
+
+		# create a list with lines containing instructions
+		code = []
+
+		for i in range(length):
+			code.append( lines[start + i + 1].replace("\n", "") )
+
+		# concat the list and divide by 2 (hexadecimal instrcutions)
+		return len( "".join(code) )/2
+
+# path to the formulas folder
+path = "/home/jtomori/Downloads/mb3d-master/M3Formulas"
+
+# find files only
+files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+# keep only .m3f files, but not JIT
+files = fnmatch.filter(files, "*.m3f")
+files = [n for n in files if not fnmatch.fnmatch(n.lower(), "*jit*")]
+
+# a list containing amount of instructions per file
+instructions = []
+
+for file in files:
+	instructions.append( countInstructions( os.path.join(path, file) ) )
+
+# generate an index list
+sortedIndices = sorted(range(len(instructions)), key=lambda k: instructions[k])
+
+# print from longest to shortest code formulas
+for index in sortedIndices[::-1]:
+	print files[index] + " (" + str(instructions[index]) + " instructions)"
