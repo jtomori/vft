55440635dd0ba26743fa2e6962c9bc68351a448f
jtomori <tomori.juraj@gmail.com>
2018-04-03 21:16:02 +0200

    added test dir with run script and scene file

---
+++ houdini/testing/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# find location of this script
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# include this project in HOUDINI_PATH
+export HOUDINI_PATH="${ROOT_DIR}:&:${HOUDINI_PATH}"
+
+# source H env
+cd /opt/hfs16.0.736/
+source houdini_setup
+cd $TEST_DIR
+
+# run the scene
+houdini testing_scene.hipnc -foreground
