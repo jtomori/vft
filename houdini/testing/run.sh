54af5d37672ef0a64568b838c2abdc62e8336c87
jtomori <tomori.juraj@gmail.com>
2018-04-04 16:48:03 +0200

    added var for running on CPU

--- houdini/testing/run.sh
+++ houdini/testing/run.sh
@@ -6,6 +6,7 @@ TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # include this project in HOUDINI_PATH
 export HOUDINI_PATH="${ROOT_DIR}:&:${HOUDINI_PATH}"
+#export HOUDINI_OCL_DEVICETYPE="CPU"
 
 # source H env
 cd /opt/hfs16.0.736/
