dbd421a20a511f732cda0334852c171766464e28
jtomori <tomori.juraj@gmail.com>
2018-06-06 12:43:21 +0200

    simplified running script

--- houdini/testing/run.sh
+++ houdini/testing/run.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 # usage
-printf "Usage:\n  -cpu argument to run OpenCL code on a CPU device (by default will run on GPU)\n\n\n"
+printf "Usage:\n  -cpu argument to run OpenCL code on a CPU device (default: GPU)\n\n\n"
 
 # find location of this script
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # include this project in HOUDINI_PATH
 export HOUDINI_PATH="${ROOT_DIR}:&:${HOUDINI_PATH}"
@@ -16,11 +15,6 @@ if [[ $1 == "-cpu" ]]
         export HOUDINI_OCL_DEVICETYPE="CPU"
         printf "CPU mode set\n"
 fi
-
-# source H env
-cd /opt/hfs16.0.736/
-source houdini_setup
-cd $TEST_DIR
 
 # run the scene
 houdini testing_scene.hipnc -foreground
