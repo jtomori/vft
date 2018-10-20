dc069360d5a94f8fccf2d467643643520cdc7943
jtomori <tomori.juraj@gmail.com>
2018-10-20 19:47:22 +0200

    adde complex plane blink kernel, added smooth coloring and orbit traps into mandelbulbrot

--- nuke/blink/include/helper_funcs.h
+++ nuke/blink/include/helper_funcs.h
@@ -5,4 +5,11 @@ static float fit(float value, float src_min, float src_max, float dst_min, float
     return dst_min + (dst_max - dst_min) * (value - src_min) / (src_max - src_min);
 }
 
+static float distPtLine(float2 pt, float2 line_pt_1, float2 line_pt_2)
+{
+    float numerator = fabs( pt.x*( line_pt_2.y - line_pt_1.y ) - pt.y*( line_pt_2.x - line_pt_1.x ) + line_pt_2.x*line_pt_1.y - line_pt_2.y * line_pt_1.x );
+    float denominator = sqrt( pow( line_pt_2.y - line_pt_1.y , 2.0f) + pow( line_pt_2.x - line_pt_1.x , 2.0f) );
+    return numerator / denominator;
+}
+
 #endif
